b1d113c1da1ff5a5d80690a70d0927e0e31ea4af
chat-me-back-up <cmc.chatme.official@gmail.com>
2024-10-25 20:31:24 +0530

    Feats:bug fix in view order deatils

--- controllers/Client/orderController.js
+++ controllers/Client/orderController.js
@@ -474,7 +474,7 @@ const cancelorder = async (req, res) => {
 
 
 //verify paymment
-const verifypayment = async (req, res) => {
+const verifyqpayment = async (req, res) => {
     try {
 
         let hmac = crypto.createHmac("sha256", process.env.KEY_SECRET);
